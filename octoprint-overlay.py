3b845d9897da06281a467fcba288052edf834de7
Will Storer <wdstorer@yahoo.com>
2021-09-10 15:20:56 -0400

    more updates to the output

--- octoprint-overlay.py
+++ octoprint-overlay.py
@@ -26,21 +26,21 @@ def getPrinterState():
   if 'Offline' in jobInfo['state']:
     print "Printer is offline"
     sys.exit()       
-  else:
-    print "Printer is online"
 
 def getStats():
   response = httpgetrequest(config.octoprinturl + "/api/job")
   jobData = json.loads(response.text)
-  print jobData['job']['file']['name']
+  if jobData['job']['file']['name']: 
+    print jobData['job']['file']['name']
+  if jobData['progress']['printTime']:
+    print "Print time: %s" % ("{}:{:02d}".format(*divmod(jobData['progress']['printTime']/60, 60)))
   if jobData['progress']['printTimeLeft']:
-    print "Print time left: %s minutes" % (jobData['progress']['printTimeLeft']/60)
+    print "%s remaining" % ("{}:{:02d}".format(*divmod(jobData['progress']['printTimeLeft']/60, 60)))
 
   response = httpgetrequest(config.octoprinturl + "/api/printer")
   printerData = json.loads(response.text)
-  if not printerData['error']:
-    print "Temperature (bed): %s" % (printerData['temperature']['bed']['actual'])
-    print "Temperature (extruder): %s" % (printerData['temperature']['tool0']['actual'])
+  print "Bed temp: %sC" % (printerData['temperature']['bed']['actual']) 
+  print "Extruder temp: %sC" % (printerData['temperature']['tool0']['actual'])
 
 getPrinterState()
 getStats()
